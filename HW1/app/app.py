326fe96756f5c17227cc8b35f47b24b63156dce8
purbanow <pawel.urbanowicz.io@gmail.com>
2019-10-13 21:06:03 +0800

    feat: add get_basic_statistics function

--- HW1/app/app.py
+++ HW1/app/app.py
@@ -3,12 +3,24 @@ import findspark
 findspark.init()
 
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 
-pre.preprocess_data('household_power_consumption')
+def get_basic_statistics(column_name):
+    max = df.agg({column_name: "max"}).collect()[0][f"max({column_name})"]
+    min = df.agg({column_name: "min"}).collect()[0][f"min({column_name})"]
+    return max,min
 
-conf = SparkConf().setAppName('hello').setMaster('spark://172.21.0.2:7077').setSparkHome('/spark/')
+conf = SparkConf().setAppName('hello')
 sc = SparkContext(conf=conf)
+spark = SparkSession(sc).builder.getOrCreate()
 
-nums= sc.parallelize([1,2,3,4])
-num = nums.take(2)
-print(num)
+df = spark.read.csv("preprocessed/pre_household_power_consumption.csv",header=True,sep=",")
+count = df.count()
+
+global_active_power_stat = get_basic_statistics("Global_active_power")
+global_reactive_power_stat = get_basic_statistics("Global_reactive_power")
+voltage_stat = get_basic_statistics("Voltage")
+global_intensity_stat= get_basic_statistics("Global_intensity")
+
+
+
