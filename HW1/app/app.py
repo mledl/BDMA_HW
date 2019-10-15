a0068233fcd6de416376f84ee162c78c2d133b8c
purbanow <pawel.urbanowicz.io@gmail.com>
2019-10-15 21:56:30 +0800

    fix: small modification to python script

--- HW1/app/app.py
+++ HW1/app/app.py
@@ -1,4 +1,3 @@
-import preprocess as pre
 import findspark
 findspark.init()
 
@@ -7,25 +6,27 @@ from pyspark.sql import SparkSession
 from pyspark.ml.feature import MinMaxScaler
 from pyspark.ml.feature import VectorAssembler
 
-def get_basic_statistics(column_name):
-    max = df.agg({column_name: "max"}).collect()[0][f"max({column_name})"]
-    min = df.agg({column_name: "min"}).collect()[0][f"min({column_name})"]
+
+def get_basic_statistics(data, column_name):
+    max = data.agg({column_name: "max"}).collect()[0][0]
+    min = data.agg({column_name: "min"}).collect()[0][0]
     return max,min
 
 # Spark setup
-conf = SparkConf().setAppName('hello')
+conf = SparkConf().setAppName('app').setMaster('spark://172.19.0.2:7077').setSparkHome('/spark/')
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc).builder.getOrCreate()
 
 # Reading csv
-df = spark.read.csv("preprocessed/pre_household_power_consumption.csv",header=True,sep=",", inferSchema=True)
+df = spark.read.csv("hdfs://namenode:8020/pre_household_power_consumption.csv",header=True,sep=",", inferSchema=True)
 
 # Calculating statistics
 count = df.count()
-global_active_power_stat = get_basic_statistics("Global_active_power")
-global_reactive_power_stat = get_basic_statistics("Global_reactive_power")
-voltage_stat = get_basic_statistics("Voltage")
-global_intensity_stat= get_basic_statistics("Global_intensity")
+
+global_active_power_stat = get_basic_statistics(df, "Global_active_power")
+# global_reactive_power_stat = get_basic_statistics(df, "Global_reactive_power")
+# voltage_stat = get_basic_statistics(df, "Voltage")
+# global_intensity_stat= get_basic_statistics(df, "Global_intensity")
 
 # Calculating Min-max normalization
 assembler = VectorAssembler(inputCols=df.columns[1:], outputCol="features")
@@ -35,8 +36,10 @@ scaler = MinMaxScaler(min=0, max=1, inputCol='features', outputCol='features_min
 scaler_model = scaler.fit(df_2)
 df_3 = scaler_model.transform(df_2)
 
-# Transforming Dense vector to dataframe
+# Transforming Dense vector to dataframe    `
 min_max_df = df_3.rdd.map(lambda x:[float(y) for y in x['features_minmax']]).toDF(df.columns[1:])
+
+print("global_active_power_stat", global_active_power_stat)
 
 
 
