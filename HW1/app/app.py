b623228784305462a7c2121b2b6057f39703b0f1
purbanow <pawel.urbanowicz.io@gmail.com>
2019-10-16 19:14:44 +0800

    fix: hadoop crush

--- HW1/app/app.py
+++ HW1/app/app.py
@@ -13,12 +13,12 @@ def get_basic_statistics(data, column_name):
     return max,min
 
 # Spark setup
-conf = SparkConf().setAppName('app').setMaster('spark://172.18.0.3:7077').setSparkHome('/spark/')
+conf = SparkConf().setAppName('app').setMaster('spark://172.20.0.2:7077').setSparkHome('/spark/')
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc).builder.getOrCreate()
 
 # Reading csv
-df = spark.read.csv("hdfs://namenode:8020/pre_household_power_consumption.csv",header=True,sep=",", inferSchema=True)
+df = spark.read.csv("hdfs://namenode:9000/data/pre_household_power_consumption.csv",header=True,sep=",", inferSchema=True)
 
 # Calculating statistics
 count = df.count()
@@ -39,7 +39,7 @@ df_3 = scaler_model.transform(df_2)
 # Transforming Dense vector to dataframe    `
 min_max_df = df_3.rdd.map(lambda x:[float(y) for y in x['features_minmax']]).toDF(df.columns[1:])
 
-print("global_active_power_stat", global_active_power_stat)
+print("global_active_power_stat", count)
 
 
 
