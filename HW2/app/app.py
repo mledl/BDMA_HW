9a1e60fc37b75f57d9497d5bcfecdec1a87c898d
Martin Ledl <martin.ledl55@gmail.com>
2019-11-07 19:57:16 +0800

    feat: lists per day/topic and mapper for co-occurrence

--- HW2/app/app.py
+++ HW2/app/app.py
@@ -1,12 +1,14 @@
 import glob
 import os
 import findspark
+import json
 
 findspark.init()
 
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import StringType, StructType, StructField, IntegerType
 from zipfile import ZipFile
 
 
@@ -35,6 +37,95 @@ def wordCountTotal(rddCol):
     words = rddCol.map(lambda r: r[0]).flatMap(lambda x: x.split(' '))
     counts = words.map(lambda w: (w, 1)).reduceByKey(lambda x, y: x + y).sortBy(lambda wc: wc[1], False)
     return counts.collect()
+
+
+def wordCountPerUnit(col, unit, df):
+    # create new column 'counts' that holds an array of all words per row (date)
+    df_timestamped_unit = df.withColumn('counts', F.struct(unit, F.split(F.col(col), ' ')))
+
+    # make ((date, word), 1) pairs and count their specific occurrence
+    rdd_count_per_unit = df_timestamped_unit.select('counts').rdd\
+        .map(lambda r: [((r[0][0], w), 1) for w in r[0][1]])\
+        .flatMap(lambda l: [x for x in l]).reduceByKey(lambda x, y: x + y)
+
+    # transform tuples to (date, (word, count)) and combine all per date and sort the tuples within the specific date
+    count_per_unit = rdd_count_per_unit.map(lambda r: (r[0][0], [(r[0][1], r[1])])).reduceByKey(lambda x, y: x + y)\
+        .map(lambda t: (t[0], sorted(t[1], key=lambda x: -x[1])))
+    return count_per_unit
+
+
+def printWordcountPerKey(data, noOfElements, isDate):
+    # data is in format [(key, [(w1, count1), ...]), ...]
+    for t1 in data:
+        i = 0
+        if isDate == 1:
+            print(t1[0].strftime('%Y/%m/%d'))
+        else:
+            print(t1[0])
+        for t2 in t1[1]:
+            print('<' + t2[0] + '> <' + str(t2[1]) + '>')
+            i += 1
+            if i > noOfElements:
+                print('\n\n')
+                break
+
+
+def generateCooccurrenceMatrices(data, df_all):
+    # specify schema of dataframe per topic
+    schema = StructType([StructField("words", StringType()), StructField("count", IntegerType())])
+
+    # to dataframe and create top 100 column
+    df_temp = data.toDF(['Topic', 'Tuples per Topic'])
+    all_tuples = df_temp.rdd.map(lambda r: (r[0], [w[0] for w in r[1]])).collect()
+    df_temp = df_temp.withColumn('Top 100', F.slice('Tuples per Topic', start=1, length=100))
+    count_tuples = df_temp.select(['Topic', 'Top 100']).rdd.map(lambda r: (r[0], [w[0] for w in r[1]])).collect()
+
+    # list of all titles per topic
+    titles_economy = df_all.select(['Title']).where(F.col('Topic').isin({'economy'})).rdd.flatMap(lambda r: r).collect()
+    df_titles_economy = spark.createDataFrame(titles_economy, "string").toDF('Title')
+    df_titles_economy = df_titles_economy.withColumn('Title', F.split(F.col('Title'), ' '))
+
+    # lists of top100 words per topic
+    economy_top100_words = count_tuples[0][1]
+    microsoft_top100_words = count_tuples[1][1]
+    palestine_top100_words = count_tuples[2][1]
+    obama_top100_words = count_tuples[3][1]
+
+    def mapStripes(sentence, top100_words):
+        stripes = {}
+        #i = 0
+        for word in top100_words:
+            h = {}
+            for neigh in neighbors(word, sentence, top100_words):
+                h[neigh] = h.get(neigh, 0) + 1
+            stripes[word] = h
+            #i += 1
+            #if i == 5:
+            #    print(sentence)
+            #    print(json.dumps(stripes))
+            #    break
+        return stripes
+
+    def neighbors(word, sentence, top100_words):
+        relevant_words = removeIrrelevantWords(top100_words, sentence)
+        return relevant_words if word in relevant_words else []
+
+    def removeIrrelevantWords(top100_words, l):
+        return list(set(l) & set(top100_words))
+
+    def reduceStripes(word, stripes):
+        result = {}
+        print(stripes)
+        for stripe in stripes:
+            result = {k: result.get(k, 0) + stripe.get(k, 0) for k in set(result) | set(stripe)}
+        return result
+
+    # calculate stripes per title
+    economy_coocurrence = df_titles_economy.rdd.map(lambda r: mapStripes(r[0], economy_top100_words))
+
+    # combine
+
+    print(json.dumps(economy_coocurrence))
 
 
 def cleanup(dir, del_dir):
@@ -104,6 +195,9 @@ tmpDir = '../data/tmp/'
 # for local testing
 extract_dataset("../data/news_popularity_dataset.zip", tmpDir)
 
+# print config
+noOfTuples = 20
+
 # Note: it is important to excape the ", bc it is used in text and otherwise the file would be split up incorrectly
 df_news = spark.read.csv(tmpDir + "News_Final.csv", header=True, sep=",", escape='"',
                          timestampFormat='yyyy-MM-dd HH:mm:ss')
@@ -120,7 +214,10 @@ df_news = df_news.fillna({'Headline': ''})
 
 # parse the timestamp in order to make time windows
 df_news = df_news.withColumn('PublishDate1', F.to_date('PublishDate', "yyyy-MM-dd HH:mm:ss"))
-df_timestamped = df_news.select(['PublishDate1', 'Title', 'Headline'])
+df_timestamped = df_news.select(['PublishDate1', 'Topic', 'Title', 'Headline'])
+
+# drop duplicates
+#df_timestamped = df_timestamped.dropDuplicates(['Title', 'Headline'])
 
 # count term frequency of title column and sort in descending order
 # in total
@@ -128,36 +225,40 @@ title_counts_total = wordCountTotal(df_timestamped.select('Title').rdd)
 headline_counts_total = wordCountTotal(df_timestamped.select('Headline').rdd)
 
 # per day
-# create timestamp word pairs
+title_counts_per_day = wordCountPerUnit('Title', 'PublishDate1', df_timestamped)
+headline_counts_per_day = wordCountPerUnit('Headline', 'PublishDate1', df_timestamped)
 
 
-# group per day
+print('###########################')
+print('  Wordcount Title per Day')
+print('###########################')
+# printWordcountPerKey(title_counts_per_day.collect(), noOfTuples, 1)
+print('###########################\n')
 
-# df_timestamped.show()
-# group by timestamp and concatenate strings using aggregation, then split into the single words
-df_timestamped_day = df_timestamped.groupBy('PublishDate1')\
-                                    .agg(F.split(F.concat_ws(' ', F.collect_list('Title')), ' ').alias('agg_title'),
-                                         F.split(F.concat_ws(' ', F.collect_list('Headline')), ' ').alias('agg_headline'))\
-                                    .sort('PublishDate1')
+print('###########################')
+print('Wordcount Headline per Day')
+print('###########################')
+# printWordcountPerKey(headline_counts_per_day.collect(), noOfTuples, 1)
+print('###########################\n')
 
-# df_timestamped_day = df_timestamped_day.withColumn('wc_pair_title',)
+# per topic
+title_counts_per_topic = wordCountPerUnit('Title', 'Topic', df_timestamped)
+headline_counts_per_topic = wordCountPerUnit('Headline', 'Topic', df_timestamped)
 
-test = df_timestamped_day.select('agg_title').rdd.map(lambda l: wordCountTotal(sc.parallelize(l))).collect()
+print('#############################')
+print('  Wordcount Title per Topic')
+print('#############################')
+# printWordcountPerKey(title_counts_per_topic.collect(), noOfTuples, 0)
+print('###########################\n')
 
-print(test[0:20])
+print('#############################')
+print('Wordcount Headline per Topic')
+print('#############################')
+# printWordcountPerKey(headline_counts_per_topic.collect(), noOfTuples, 0)
+print('###########################\n')
 
-# df_timestamped_day.show()
-
-# words_per_day = df_timestamped.rdd.map(lambda )
-
-
-# output = title_counts_total
-# i = 1
-# for (word, count) in output:
-#    print("%s: %i" % (word, count))
-#    if i == 10:
-#        break
-#    i += 1
+# co-occurrence matrices for Title and Headline for top 100 words per Topic
+generateCooccurrenceMatrices(title_counts_per_topic, df_timestamped)
 
 
 # for local testing
