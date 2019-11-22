5e9c36dc2751d13491459a84d68c0c2467c080b8
Tobias Kick <kick.tobias@gmx.de>
2019-11-22 11:45:17 +0800

    HW3 part 1 now working :)

--- HW3/app/app3.py
+++ HW3/app/app3.py
@@ -1,15 +1,13 @@
 import findspark
 findspark.init()
-from collections import defaultdict
-from typing import Tuple, List
 
-import numpy as np
-import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import regexp_replace, split, lower, col
 from pyspark.sql.types import *
 import pyspark.sql.functions as f
+from pyspark.sql.functions import row_number, monotonically_increasing_id
+from pyspark.sql import Window
 
 #######################################################################################################################
 
@@ -18,7 +16,7 @@ run_spark_in_cluster = False      # SET THIS VARIABLE FOR TESTING VS PRODUCTION
 k = 5  # parameter for number of k-shingles
 
 link_to_cluster_storage = "hdfs://namenode:9000"
-link_to_local_storage = "C:/Users/Tobias/Docker/BDMA_HW"
+link_to_local_storage = "../data/"
 if(run_spark_in_cluster):
     path_to_write = ""
 else:
@@ -27,52 +25,79 @@ else:
 #######################################################################################################################
 
 def shingling(element, k):
-    elem = np.array(element)
-    for i in range(elem[0].size - k + 1):
+    for i in range(len(element[0]) - k + 1):
         shingle = ""
+        doc_nr = int(element[1])
         for j in range(i, i+k):
-            if j < (i+k):
-                shingle = shingle + str(elem[0][j]) + " "
+            if j < (i+k-1):
+                shingle = shingle + str(element[0][j]) + " "
             else:
-                shingle = shingle + str(elem[0][j])
-        yield shingle    # yield can return a sequence of objects/results
+                shingle = shingle + str(element[0][j])
+        yield shingle, doc_nr   # key, value     # yield can return a sequence of objects/results
+
+
+def convert_Output(x, doc_count):
+    out = []
+    out.append(x[0])
+    index = 0
+    list_len = len(x[1])
+
+    for i in range(doc_count):   # iterating through from 0 to (doc_count -1)
+        if index < list_len:
+            doc = int(x[1][index])     # document number
+            if doc == i:
+                out.append(1)
+                index = index + 1
+            else:
+                out.append(0)
+        else:
+            out.append(0)
+    return out
 
 
 #######################################################################################################################
 
 if(run_spark_in_cluster):
-    spark = SparkSession.builder.appName('hw2').master('spark://spark-master:7077').getOrCreate()
-    sqlContext = SQLContext(spark)
-    # infer Schema important in order to realize that numbers a not a string
-    #df = spark.read.csv(link_to_cluster_storage + "/household_power_consumption.csv", sep=",", inferSchema=True, header=True)
-else:
-    spark = SparkSession.builder \
-        .appName('hw3') \
-        .master('local') \
+    spark = SparkSession.builder.appName('hw3').master('spark://spark-master:7077') \
         .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.7.0") \
-        .config("spark.driver.maxResultSize", "20g") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
+    df = spark.read \
+        .format("com.databricks.spark.xml") \
+        .option("rootTag", "<!DOCTYPE") \
+        .option("rowTag", "BODY") \
+        .load(link_to_cluster_storage + "test/*")
+else:
+    spark = SparkSession.builder.appName('hw3').master('local') \
+        .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.7.0") \
+        .getOrCreate()
+    sqlContext = SQLContext(spark)
+    df = spark.read \
+        .format("com.databricks.spark.xml") \
+        .option("rootTag", "<!DOCTYPE") \
+        .option("rowTag", "BODY") \
+        .load(path_to_write + "test/*")
 
-df = spark.read \
-    .format("com.databricks.spark.xml") \
-    .option("rootTag", "<!DOCTYPE") \
-    .option("rowTag", "BODY") \
-    .load(path_to_write + "/HW3/data/*")
 
 ### DATA CLEANING
 df = df.withColumn('content', regexp_replace('_corrupt_record', '\n', ' ')) # replace newline by one whitespace
 df = df.select('content').withColumn('content', lower(col('content'))) # text to lowercase
 df = df.withColumn('content', regexp_replace('content', '<body>', ''))  # replace <BODY>
 df = df.withColumn('content', regexp_replace('content', '[\\?,\\.,\\$]', ' ')) # replace ? , . and $ by whitespace
+df = df.withColumn('content', regexp_replace('content', '\r', ' '))
 df = df.withColumn('content', regexp_replace('content', ' +', ' ')) # replace multiple whitespaces by one
 df = df.withColumn('content', regexp_replace('content', ' reuter &#3;</body>', '')) # replace BODY tag at end of document
 df_split = df.withColumn('content', split(col('content'), " ").alias('content'))
+df_split_id = df_split.withColumn("id", row_number().over(Window.orderBy(monotonically_increasing_id()))-1)
 
 
-### CREATE SHINGLES
-result_shingle = df_split.rdd.flatMap(lambda x: shingling(x, k)).distinct()
-document_list = df.collect()
+### CREATE SHINGLE SET REPRESENTATION
+result_shingle = df_split_id.rdd.flatMap(lambda x: shingling(x, k)).distinct() \
+    .groupByKey() \
+    .map(lambda x: (x[0], list(x[1])))   # grouping the values together for each unique shingle
 
-df_matrix = result_shingle.map(lambda x: ["1" if str(x) in str(document) else "0" for document in document_list]).toDF()
-df_matrix.coalesce(1).write.format('com.databricks.spark.csv').mode('overwrite').save(path_to_write + "/HW3/data/results/task1.csv", header='true', sep=" ")
+doc_count = df.count()
+print("Number of documents: " + str(doc_count))
+
+df_matrix = result_shingle.map(lambda x: convert_Output(x, doc_count)).toDF()
+df_matrix.coalesce(1).write.format('com.databricks.spark.csv').mode('overwrite').save(path_to_write + "results/task1", header='true', sep=" ")
