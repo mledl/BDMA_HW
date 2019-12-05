2b40046d3953b48c5f5fc1df8220f7ea7e512468
Martin Ledl <martin.ledl55@gmail.com>
2019-12-05 22:33:37 +0800

    added pivoted table for user-user

--- HW4/app/app.py
+++ HW4/app/app.py
@@ -3,8 +3,10 @@ findspark.init()
 
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import count, sum, desc
-from pyspark.sql.types import StringType
+from pyspark.sql.functions import count, sum, desc, col, expr
+from sklearn.metrics.pairwise import cosine_similarity
+import databricks.koalas as ks
+import numpy as np
 
 
 #######################################################################################################################
@@ -33,6 +35,51 @@ def toprated_movies_based_on_average_rating(df):
         .format('com.databricks.spark.csv')\
         .mode('overwrite')\
         .save(path_to_write + "/task1", header='true', sep=',')
+
+
+def top_similar_users_old(df_r):
+    # normalize the user ratings by user means
+    df_user_avg = df_r.groupBy('UserID')\
+                        .agg(sum('Rating')/count('Rating'))\
+                        .withColumnRenamed('(sum(Rating) / count(Rating))', 'avg_rating')
+
+    df_r = df_r.alias("df1").join(df_user_avg.alias("df2"), df_r.UserID == df_user_avg.UserID)\
+        .select("df1.UserID", "df1.MovieID", "df1.Rating", "df2.avg_rating")\
+        .withColumn("norm_rating", col('Rating') - col('avg_rating'))\
+        .select("UserID", "MovieID", "norm_rating")
+
+    # create pivot matrix UserID/MovieID with normalized ratings as values
+    ks_user_movie = ks.DataFrame(df_r).pivot_table(values='norm_rating', index=['UserID'],
+                                                   columns='MovieID')
+    # replacing NaN by movie avg
+    ks_movie = ks_user_movie.fillna(ks_user_movie.mean(axis=0))
+
+    # replacing NaN by user avg
+    ks_user = ks_user_movie.applymap(lambda r: r.fillna(r.mean()))
+
+    # calculate the user similarity
+    cosine_sim = cosine_similarity(ks_user_movie)
+    np.fill_diagonal(cosine_sim, 0)
+    user_similarity = ks.DataFrame(cosine_sim, index=ks_user_movie.index)
+    user_similarity.columns = ks_user.index
+
+    print(user_similarity)
+
+
+def top_similar_users(df_r, user):
+    # normalize the user ratings by user means
+    df_user_avg = df_r.groupBy('UserID') \
+        .agg(sum('Rating') / count('Rating')) \
+        .withColumnRenamed('(sum(Rating) / count(Rating))', 'avg_rating')
+
+    df_r = df_r.alias("df1").join(df_user_avg.alias("df2"), df_r.UserID == df_user_avg.UserID) \
+        .select("df1.UserID", "df1.MovieID", "df1.Rating", "df2.avg_rating") \
+        .withColumn("norm_rating", col('Rating') - col('avg_rating')) \
+        .select("UserID", "MovieID", "norm_rating")
+
+    # create pivot matrix UserID/MovieID with normalized ratings as values
+    ks_user_movie = ks.DataFrame(df_r).pivot_table(values='norm_rating', index=['UserID'], columns='MovieID')
+    print(ks_user_movie.head(30))
 
 #######################################################################################################################
 
@@ -76,5 +123,11 @@ df_movies = spark.sparkContext.textFile('../data/movies.dat')\
 
 # sub task 1: list the top-rated movies based on the ‘average’ rating score.
 # (sorted in descending order of ‘average’ rating score)
-toprated_movies_based_on_average_rating(df_ratings)
+#toprated_movies_based_on_average_rating(df_ratings)
+
+# sub task 2: Given any user, please list the top-’similar’ users based on
+# the cosine similarity of previous ratings each user has given.
+# (sorted in descending order of ‘user’ similarity score)
+top_similar_users(df_ratings, 1)
+
 
