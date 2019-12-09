1dd51e7de6aa1170b9913d78679b7e1dc79e6121
Martin Ledl <martin.ledl55@gmail.com>
2019-12-09 16:22:45 +0800

    feat: added task2 similarity score for users

--- HW4/app/app.py
+++ HW4/app/app.py
@@ -66,7 +66,7 @@ def top_similar_users_old(df_r):
     print(user_similarity)
 
 
-def top_similar_users(df_r, user):
+def top_similar_users_pandas(df_r, user):
     # normalize the user ratings by user means
     df_user_avg = df_r.groupBy('UserID') \
         .agg(sum('Rating') / count('Rating')) \
@@ -78,8 +78,55 @@ def top_similar_users(df_r, user):
         .select("UserID", "MovieID", "norm_rating")
 
     # create pivot matrix UserID/MovieID with normalized ratings as values
-    ks_user_movie = ks.DataFrame(df_r).pivot_table(values='norm_rating', index=['UserID'], columns='MovieID')
-    print(ks_user_movie.head(30))
+    # fill with 0 because it is faster
+    df_user_movie_ratings = df_r.groupBy("UserID")\
+        .pivot("MovieID")\
+        .agg(expr("coalesce(first(norm_rating),0)").cast("double")).fillna(0)
+
+    # make df for the given user and one for all the others
+    df_user = df_user_movie_ratings.filter(df_user_movie_ratings.UserID == user)
+    df_other_users = df_user_movie_ratings.filter(df_user_movie_ratings.UserID != user)
+
+    # cosine similarity between dedicated user and all the others
+    pd_user = df_user.toPandas()
+    pd_other_users = df_other_users.toPandas()
+    cos_similarities = cosine_similarity(pd_user, pd_other_users)[0]
+    cos_similarities = [float(cos_sim) for cos_sim in cos_similarities]
+
+    # create output format
+    user_similarity = dict(zip(pd_other_users['UserID'], cos_similarities))
+
+    # sort descending by similarity
+    user_similarity = sorted(user_similarity.items(), key=lambda x: x[1])
+    user_similarity.reverse()
+
+    # make df and write to file
+    df_user_similarity = sqlContext.createDataFrame(user_similarity, ['user', 'similarity'])
+    df_user_similarity.show()
+    df_user_similarity \
+        .coalesce(1) \
+        .write \
+        .format('com.databricks.spark.csv') \
+        .mode('overwrite') \
+        .save(path_to_write + "/task2", header='true', sep=',')
+
+
+def top_similar_movies(df_r, movie):
+    # normalize the movie ratings by movie means
+    df_movie_avg = df_r.groupBy('MovieID')\
+        .agg(sum('Rating')/count('Rating'))\
+        .withColumnRenamed('(sum(Rating) / count(Rating))', 'avg_rating')
+
+    df_r = df_r.alias("df1").join(df_movie_avg.alias("df2"), df_r.MovieID == df_movie_avg.MovieID) \
+        .select("df1.UserID", "df1.MovieID", "df1.Rating", "df2.avg_rating") \
+        .withColumn("norm_rating", col('Rating') - col('avg_rating')) \
+        .select("UserID", "MovieID", "norm_rating")
+
+
+
+
+
+
 
 #######################################################################################################################
 
@@ -98,6 +145,11 @@ user_cols = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
 movie_cols = ['MovieID', 'Title', 'Genres']
 
 df_ratings = spark.sparkContext.textFile('../data/ratings.dat')\
+    .map(lambda x: x.split("::"))\
+    .toDF()\
+    .toDF(*rating_cols)
+
+df_ratings_5user = spark.sparkContext.textFile('../data/ratings_5user.dat')\
     .map(lambda x: x.split("::"))\
     .toDF()\
     .toDF(*rating_cols)
@@ -128,6 +180,13 @@ df_movies = spark.sparkContext.textFile('../data/movies.dat')\
 # sub task 2: Given any user, please list the top-’similar’ users based on
 # the cosine similarity of previous ratings each user has given.
 # (sorted in descending order of ‘user’ similarity score)
-top_similar_users(df_ratings, 1)
+# calculate similarities to user #1 and store data to csv in format <user, similarity>
+top_similar_users_pandas(df_ratings, 1)
+
+
+# sub task 3: Given any movie, please list the top-’similar’ movies based on
+# the cosine similarity of previous ratings each movie received.
+# (sorted in descending order of ‘item’ similarity score)
+#top_similar_movies(df_ratings, 2000)
 
 
